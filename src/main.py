bb542fb8272168b282c53968db63dc59776263d5
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2023-09-10 23:38:09 +0800

    test: gradio GUI

--- src/main.py
+++ src/main.py
@@ -1,37 +1,69 @@
-from typing import List
+import gradio as gr
 
-from langchain.schema import Document
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
 
 from paths import data_file_path, db_dir
-from utils.faiss_db import create_db, db_save_local, \
-    db_get_answers_from_query, db_get_top_k_answers
+from utils.faiss_db import create_db, db_save_local
 
 
-def print_docs(docs: List[Document]):
-    for doc in docs:
-        print(doc.page_content, '\n', sep='')
-
-
-def main():
+def init_data(data_file_path: str, db_dir: str):
     db = create_db(data_file_path)
     db_save_local(db, db_dir)
 
-    query = ('留学')
 
-    # 1. Get answers for query
-    answers = db_get_answers_from_query(db, query)
-    print(f'[1. Get answers for query: "${query}"]')
-    print_docs(answers)
+class CounsellingChatBot:
+    def __init__(self, pkl_data_dir: str = db_dir, enable_chat: bool = True):
+        db = FAISS.load_local(pkl_data_dir, OpenAIEmbeddings())
+        llm = ChatOpenAI(model_name='gpt-3.5-turbo', temperature=0)
 
-    print('\n')
+        self.SALES_BOT = RetrievalQA.from_chain_type(
+            llm,
+            retriever=db.as_retriever(
+                search_type='similarity_score_threshold',
+                search_kwargs={
+                    'score_threshold': 0.8
+                },
+            ),
+        )
 
-    # 2. Get top k answers for query
-    k = 5
-    print(f'[2. Get top {k} answers for query: "{query}"]')
-    answers = db_get_top_k_answers(db, k, query)
-    print_docs(answers)
+        self.SALES_BOT.return_source_documents = True
 
-    print('\n')
+        self.enable_chat = enable_chat
+
+
+sales_bot = CounsellingChatBot(pkl_data_dir=db_dir)
+
+
+def chat(message: str, history: str):
+    print(f'[message]{message}')
+    print(f'[history]{history}')
+
+    ans = sales_bot.SALES_BOT({'query': message})
+
+    if ans['source_documents'] or sales_bot.enable_chat:
+        print(ans['result'])
+        print(ans['source_documents'])
+        return ans['result']
+    else:
+        return '这个问题我要问问领导'
+
+
+def launch_gradio():
+    demo = gr.ChatInterface(
+        fn=chat,
+        title='留学咨询',
+        chatbot=gr.Chatbot(height=600),
+    )
+
+    demo.launch(share=True, server_name='localhost')
+
+
+def main():
+    init_data(data_file_path, db_dir)
+    launch_gradio()
 
 
 if __name__ == '__main__':
--- src/paths.py
+++ src/paths.py
@@ -10,3 +10,4 @@ db_dir = os.path.join(
     os.path.dirname(__file__),
     '../data/faiss.db'
 )
+db_faiss_file = os.path.join(db_dir, 'index.faiss')
