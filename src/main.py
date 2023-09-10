d99dc672fbaf7b399019660d49dee71080e0ea8e
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2023-09-10 22:31:42 +0800

    test: hard-coded info

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 import os.path
 
-from utils.faiss_db import create_faiss_db, db_get_answers_from_query
+from typing import List
+
+from langchain.schema import Document
+
+from utils.faiss_db import create_faiss_db, db_get_answers_from_query, \
+    db_get_top_k_answers
 from utils.split_text import split_file_as_documents
+
+
+def print_docs(docs: List[Document]):
+    for doc in docs:
+        print(doc.page_content, '\n', sep='')
 
 
 def main():
@@ -14,9 +24,21 @@ def main():
     db = create_faiss_db(documents)
 
     query = ('留学')
-    answers = db_get_answers_from_query(db, query)
 
-    print(answers)
+    # 1. Get answers for query
+    answers = db_get_answers_from_query(db, query)
+    print(f'[1. Get answers for query: "{query}"]')
+    print_docs(answers)
+
+    print('\n')
+
+    # 2. Get top k answers for query
+    k = 5
+    print(f'[2. Get top {k} answers for query: "{query}"]')
+    answers = db_get_top_k_answers(db, k, query)
+    print_docs(answers)
+
+    print('\n')
 
 
 if __name__ == '__main__':
