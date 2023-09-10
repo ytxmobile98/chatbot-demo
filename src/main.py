9b1da96569a2997f724cacc83c968d80deb494dd
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2023-09-10 22:59:40 +0800

    refactor: import_data

--- src/main.py
+++ src/main.py
@@ -1,12 +1,9 @@
-import os.path
-
 from typing import List
 
 from langchain.schema import Document
 
-from utils.faiss_db import create_faiss_db, db_get_answers_from_query, \
-    db_get_top_k_answers
-from utils.split_text import split_file_as_documents
+from utils.faiss_db import db_get_answers_from_query, db_get_top_k_answers
+from utils.import_data import create_db
 
 
 def print_docs(docs: List[Document]):
@@ -15,19 +12,13 @@ def print_docs(docs: List[Document]):
 
 
 def main():
-    data_file = os.path.join(
-        os.path.dirname(__file__),
-        '../data/sample-answers.txt'
-    )
-    documents = split_file_as_documents(data_file)
-
-    db = create_faiss_db(documents)
+    db = create_db()
 
     query = ('留学')
 
     # 1. Get answers for query
     answers = db_get_answers_from_query(db, query)
-    print(f'[1. Get answers for query: "{query}"]')
+    print(f'[1. Get answers for query: "${query}"]')
     print_docs(answers)
 
     print('\n')
