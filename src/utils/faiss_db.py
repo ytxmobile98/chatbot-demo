95ea78866a05e424fdd6d0c877eff72dbf2c7da5
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2023-09-10 22:31:26 +0800

    feat: get top K answers

--- src/utils/faiss_db.py
+++ src/utils/faiss_db.py
@@ -15,3 +15,9 @@ def create_faiss_db(documents: List[Document],
 def db_get_answers_from_query(db: FAISS, query: str) -> List[Document]:
     answers = db.similarity_search(query)
     return answers
+
+
+def db_get_top_k_answers(db: FAISS, k: int, query: str) -> List[Document]:
+    top_k_retriever = db.as_retriever(search_kwargs={'k': k})
+    docs = top_k_retriever.get_relevant_documents(query)
+    return docs
